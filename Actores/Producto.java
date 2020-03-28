bd978b0bc1062aaee3bd09233ad55979fb634bf0
Omar <Contac@gmail.com>
2020-03-28 14:49:46 -0600

    Registro de productos

--- Actores/Producto.java
+++ Actores/Producto.java
@@ -8,5 +8,28 @@ package Actores;
  * @author omara
  */
 public class Producto {
-    
+   int codigoBarras;
+   String NombreP;
+   double PrecioUnitario,PrecioMayoreo;
+   double unidades;
+   Producto siguinte;
+
+    public Producto(int codigoBarras, String NombreP, double PrecioUnitario, double PrecioMayoreo, double unidades, Producto siguinte) {
+        this.codigoBarras = codigoBarras;
+        this.NombreP = NombreP;
+        this.PrecioUnitario = PrecioUnitario;
+        this.PrecioMayoreo = PrecioMayoreo;
+        this.unidades = unidades;
+        this.siguinte = siguinte;
+    }
+
+    public Producto(int codigoBarras, String NombreP, double PrecioUnitario, double PrecioMayoreo, double unidades) {
+        this.codigoBarras = codigoBarras;
+        this.NombreP = NombreP;
+        this.PrecioUnitario = PrecioUnitario;
+        this.PrecioMayoreo = PrecioMayoreo;
+        this.unidades = unidades;
+    }
+   
+   
 }
