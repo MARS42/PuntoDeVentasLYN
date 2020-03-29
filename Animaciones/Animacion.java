473c31fbe1eb8448ccd342491d7447bbf064946d
Robert Mtz <msjred42@gmail.com>
2020-03-28 19:20:29 -0600

    Animación Minimizar/Maximizar de la ventana Principal

--- Animaciones/Animacion.java
+++ Animaciones/Animacion.java
@@ -4,6 +4,7 @@ import Ventanas.Login;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.Callable;
 import javax.swing.Timer;
 
 /**
@@ -14,11 +15,15 @@ public class Animacion implements ActionListener
 {
     private final Timer timer;  
     private float lerp = 0f;;
+    public float getLerp(){ return lerp; }
     private final Transform origin_transform;                               //Estado original del componente
     private final Transform from_transform;                                 //Estado inicial del componente
     private final Transform to_transform;                                   //A donde se moverá y escalará el componente
     private Transform current_transform;                                    //Estado de la transición 
     private final Component objetivo;
+    private final Callable<Void> action;                                    //Método a llamar al terminar animación
+    private Callable<Void> updateAction;                                    //Método que se llamará a cada actualización 
+    public void setUpdateAction(Callable<Void> updateAction){ this.updateAction = updateAction; }
     
     /***
      * Inicia una animación que dará como resultado un desplazamiento o escalonamiento
@@ -27,10 +32,12 @@ public class Animacion implements ActionListener
      * @param newpy Valor objetivo de posición en eje Y
      * @param newsx Valor objetivo de escala en eje X
      * @param newsy Valor objetivo de escala en eje Y
+     * @param action Método que se llamará al final de la animación
      */
-    public Animacion(Component o, int newpx, int newpy, int newsx, int newsy)
+    public Animacion(Component o, int newpx, int newpy, int newsx, int newsy, Callable<Void> action)
     {
         this.objetivo = o;
+        this.action = action;
         origin_transform = new Transform(o.getX(), o.getY(), o.getWidth(), o.getHeight());
         from_transform = new Transform(origin_transform.px, origin_transform.py, origin_transform.sx, origin_transform.sy);
         to_transform = new Transform(newpx, newpy, newsx, newsy);
@@ -65,17 +72,22 @@ public class Animacion implements ActionListener
     
     private void Animar()
     {
-        if(lerp <= 1f)
-        {
-            current_transform = Controlador.main.LerpTransform(from_transform, to_transform, lerp, current_transform);
-            objetivo.setLocation(current_transform.px, current_transform.py);
-            objetivo.setSize(current_transform.sx, current_transform.sy);
-            lerp += Controlador.main.velocidadAnim;
-            Login.ins.pack();
+        try {
+            if(lerp <= 1f)
+            {
+                current_transform = Controlador.main.LerpTransform(from_transform, to_transform, lerp, current_transform);
+                objetivo.setLocation(current_transform.px, current_transform.py);
+                objetivo.setSize(current_transform.sx, current_transform.sy);
+                lerp += Controlador.main.velocidadAnim;
+                Login.ins.pack();
+                updateAction.call();
+            }
+            else
+            {
+                action.call();
+                timer.stop();
+            }
         }
-        else
-        {
-            timer.stop();
-        }
+        catch (Exception ex) { }
     }
 }
